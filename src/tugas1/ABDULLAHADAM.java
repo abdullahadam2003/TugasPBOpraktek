06a8c72ef079614a92378330dab6c29d05a5609e
abdullahadam2003 <abdullahadam2017@gmail.com>
2023-03-25 11:09:48 +0700

    first

---
+++ src/tugas1/ABDULLAHADAM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author windows
+ */
+public class ABDULLAHADAM {
+    
+
+public static void main(String[] args ){
+       System.out.println("Welcome To Java Programming ABDULLAH ADAM");
+}
+}
+   
