db91a894074656c5c6c34f7dd8f34b412c449167
abdullahadam2003 <abdullahadam2017@gmail.com>
2023-04-19 21:28:38 +0700

    upload

---
+++ src/tugas 2/latihan71forloop.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul7;
+
+/**
+ *
+ * @author windows
+ */
+public class latihan71forloop {
+     public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+
+        int i=0;
+        do
+            {
+                System.out.println(hari[i]);
+                i++;
+            }while(i<hari.length);
+    }
+    
+}
+
+    
+
